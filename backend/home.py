ea04e40dcfdab1adba85f05463abfd876542c38b
Ivan <ijaimes2872@gmail.com>
2019-11-09 14:02:39 -0600

    home flask

--- backend/home.py
+++ backend/home.py
@@ -2,8 +2,22 @@ from flask import Flask, escape, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    # name = request.args.get("name", "World")
+    name = "bob"
+    if request.method == "POST":
+        name = "LOL"
 
+    return name
+    # return "Hello, {escape(name)}!"
+
+
+
+
+
+
+
+
+# To Run
+# env FLASK_APP=home.py flask run
