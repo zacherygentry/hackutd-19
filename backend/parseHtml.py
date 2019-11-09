af0403841870dc3054b47638e9f918cdd3ec94e3
Ivan <ijaimes2872@gmail.com>
2019-11-09 17:35:56 -0600

    added BS4

--- backend/parseHtml.py
+++ backend/parseHtml.py
@@ -1,17 +1,35 @@
 from bs4 import BeautifulSoup
 
 
-soup = BeautifulSoup(html_doc, './example_fox.json')
+# soup = BeautifulSoup(html_doc, './example_fox.json')
 
-print(soup.prettify())
+# print(soup.prettify())
+
+class ParseHtml:
+    def __init__(self, data):
+        self.html_doc = data
+        self.soup = BeautifulSoup(self.html_doc, "lxml")
+
+    def generate(self):
+        for p in self.soup.find_all('p'):
+            self.fileWriter(p.get_text() + "\n")
+            print("[" + p.get_text() + "]")
+
+    def fileWriter(self, data):
+        with open('paragraphExample.txt', 'a') as file:
+            file.write(data)
 
 
 
 
 
 
+htmlData = ""
+with open('example_nyt.json', 'r') as file:
+    htmlData = file.read()
 
-
+parser = ParseHtml(htmlData)
+parser.generate()
 
 
 
